4c7fc12eb95cb977f699b8508c39045b4638bb73
Dave Edelhart <dedelhart@leapmotion.com>
2014-02-07 13:01:22 -0800

    got image load working

--- DemoDirectory/demo.js
+++ DemoDirectory/demo.js
@@ -2,6 +2,7 @@ var loader = new THREE.ImageLoader();
 
 loader.load('https://rawgithub.com/bingomanatee/fiddletest/master/DemoDirectory/images/dave_trek.jpg', function (data) {
     console.log('loaded: ', data);
+    document.getElementById('foo').appendChild(data);
 }, function () {
 
 }, function (err) {
